e422df72d9ade17e8842e0265da712698b7d5093
MatthewHunt84 <matthew.hunt@wundermanthompson.com>
2024-04-25 14:44:59 -0600

    try again

---
+++ DeleteMe/NewClass.swift
@@ -0,0 +1,14 @@
+//
+//  NewClass.swift
+//  DeleteMe
+//
+//  Created by Matt Hunt on 4/25/24.
+//
+
+import Foundation
+
+class NewClass {
+    func doThing() {
+        print("I'm a framework method")
+    }
+}
