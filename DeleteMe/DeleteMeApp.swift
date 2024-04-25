3e7d6e0f7b7efac62fa72817970483bf59aac4c2
MatthewHunt84 <matthew.hunt@wundermanthompson.com>
2024-04-25 14:29:59 -0600

    initial commit

---
+++ DeleteMe/DeleteMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeleteMeApp.swift
+//  DeleteMe
+//
+//  Created by Matt Hunt on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DeleteMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
